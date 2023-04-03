be9095e8d5fb4c90ad606fcf0342eaac788ab131
0xATHERIS <ath3ris@proton.me>
2023-04-04 00:36:21 +0100

    feat: sparkles 3 - User can have a commit schema
    Commit Schema helps the user structure their commit message and they between <> will be replaced with the value they entered

--- src/extension.ts
+++ src/extension.ts
@@ -14,26 +14,33 @@ import emojiQuickPick from './steps/step_3_emoji/emojiQuickPick';
 
 export function activate(context: vscode.ExtensionContext) {
   	const disposable = vscode.commands.registerCommand('simple-commit.commit', async () => {
+		// === INITIATE GIT ===
 		const git_extension = vscode.extensions.getExtension('vscode.git')?.exports;
 		const repo = git_extension.getAPI(1).repositories[0];
 
-		// === AUTO-DETECT ISSUE NUMBER
+		// === INITIATE CONFIG ===
+		const workspace_config = vscode.workspace.getConfiguration('simpleCommit');
+
+		// === AUTO-DETECT ISSUE NUMBER ===
 		const head = repo.state.HEAD;
 		const repo_name: string = head.name;
-		const possible_issue_number = repo_name.match(/^([^\d]*)(\d+)/);
+		const possible_issue_number = repo_name.match(/(?!.*\/)([^\d]*)(\d+)/);
 
 		let issue_number = '';
 		if (possible_issue_number) {
 			issue_number = possible_issue_number[0];
 		}
 
+		// === GET SCHEMA ===
+		const DEFAULT_SCHEMA =  "<type>(<scope>): <emoji> <number> - <description>\n\n<body>\n\n<footer>";
+		const workspace_schema = workspace_config.get('schema') as unknown as string;
+
 		// === TYPE ===
-		let type = null;
+		let type = '';
 		type = await typeQuickPick();
 
 		// === SCOPE ===
 		let scope = null;
-		const workspace_config = vscode.workspace.getConfiguration('simpleCommit');
 		const workspace_scopes: string[] = workspace_config.get('scopes') as unknown as string[];
 
 		const scope_type = await scopeQuickPick(workspace_scopes);
@@ -46,7 +53,11 @@ export function activate(context: vscode.ExtensionContext) {
 			})
 			.then(async (new_scope) => {
 				if (new_scope) {
-					await workspace_config.update('scopes', [...workspace_scopes, new_scope], vscode.ConfigurationTarget.Workspace)
+					await workspace_config.update(
+						'scopes',
+						[...workspace_scopes, new_scope],
+						vscode.ConfigurationTarget.Workspace,
+					)
 						.then(() => {
 							if (workspace_scopes) {
 								scope = [...workspace_scopes, new_scope];
@@ -57,8 +68,7 @@ export function activate(context: vscode.ExtensionContext) {
 					scope = new_scope;
 				}
 			});
-		}
-		if (scope_type === 'Once Time Scope') {
+		} else if (scope_type === 'Once Time Scope') {
 			await vscode.window.showInputBox({
 				prompt: 'Enter scope name.',
 				title: 'One Time Scope',
@@ -69,69 +79,67 @@ export function activate(context: vscode.ExtensionContext) {
 					scope = new_scope;
 				}
 			});
+		} else if (scope_type === 'None') {
+			scope = '';
+		} else {
+			// User chose an existing saved scope.
+			scope = scope_type;
 		}
 
 		// === EMOJI ===
 		const emoji = await emojiQuickPick();
 
 		// === ISSUE/TICKET NUMBER ===
-		let ticket_number = null;
+		let ticket_number = '';
         ticket_number = await vscode.window.showInputBox({
 			prompt: 'Enter issue or ticket number',
 			title: 'Issue/Ticket Number',
 			placeHolder: 'Enter you issue or ticket number.',
 			value: issue_number,
-		});
+		}) as string;
 
 		// === DESCRIPTION ===
-		let description = null;
+		let description = '';
         description = await vscode.window.showInputBox({
 			prompt: 'Enter a short description of this commit.',
 			title: 'Description',
 			placeHolder: 'Enter a short description of this commit.',
-		});
+		}) as string;
 		
 		// === BODY ===
-		let body = null;
+		let body = '';
         body = await vscode.window.showInputBox({
 			prompt: 'Enter a detailed description of this commit.',
 			title: 'Body',
 			placeHolder: 'Enter a detailed description of this commit.',
-		});
+		}) as string;
 
 		// === FOOTER ===
-		let footer = null;
+		let footer = '';
         footer = await vscode.window.showInputBox({
 			prompt: 'Enter a footer',
 			title: 'Footer',
 			placeHolder: 'Enter remaining information such as Breaking Changes details about this commit.',
-		});
+		}) as string;
 
 		// === BUILDING COMMIT MESSAGE ===
-		const commit_array: string[] = [];
-
-		const t = type;
-		const s = scope ? `(${scope})` : '';
-		const tn = ticket_number ? `${ticket_number} - ` : '';
-		const d = description;
-		const first = `${t}${s}: ${emoji} ${tn}${d}`;
-		commit_array.push(first);
-
-		if (body) {
-			commit_array.push(body);
+		let chosen_schema = '';
+		if (workspace_schema) {
+			chosen_schema = workspace_schema;
+		} else {
+			chosen_schema = DEFAULT_SCHEMA;
 		}
-
-		if (footer) {
-			commit_array.push(footer);
-		}
-
-		console.log(vscode.extensions.getExtension('vscode.git'));
-
+		const commit_message = chosen_schema.replace('<type>', type)
+										.replace('<scope>', scope ? `(${scope})` : '')
+										.replace('<emoji>', emoji)
+										.replace('<number>', ticket_number)
+										.replace('<description>', description)
+										.replace('<body>', body)
+										.replace('<footer>', footer);
 		// === COMMIT ===
-		repo.commit(commit_array.join('\n\n'))
-			.then((res: unknown) => {
-				console.log(res);
-				vscode.window.showInformationMessage(`Commit made.\n\n${commit_array.join('\n\n')}`);
+		repo.commit(commit_message)
+			.then(() => {
+				vscode.window.showInformationMessage(`Commit made.\n\n${commit_message}`);
 			})
 			.catch((err: { message: string, stdout: string }) => {
 				console.error(err);
