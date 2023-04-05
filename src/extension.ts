d5dc03a2e08fcb6501da63e24c78d6a5e0b91088
0xATHERIS <ath3ris@proton.me>
2023-04-06 00:06:44 +0100

    feat: :sparkles: 9 - Implement the Back Button functionality. Uses regex to get the step order.
    Regex matches all of the steps and uses the template to get the step order. It then loops over the step order to show each prompt, when the back button is pressed, the loop index goes back one to show previous prompt.

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,11 @@
 import * as vscode from 'vscode';
-import typeQuickPick from './steps/step_1_type/typeQuickPick';
-import scopeQuickPick from './steps/step_2_scope/scopeQuickPick';
-import emojiQuickPick from './steps/step_3_emoji/emojiQuickPick';
-import numberInputBox from './steps/step_4_ticket_number/numberInputBox';
-import descriptionInputBox from './steps/step_5_description/descriptionInputBox';
-import bodyInputBox from './steps/step_6_body/bodyInputBox';
-import footerInputBox from './steps/step_7_footer/footerInputBox';
-import newScopeInputBox from './steps/step_2_scope/newScopeInputBox';
-import oneTimeScopeInputBox from './steps/step_2_scope/oneTimeScopeInputBox';
+import typeQuickPick from './steps/type/typeQuickPick';
+import emojiQuickPick from './steps/emoji/emojiQuickPick';
+import numberInputBox from './steps/issue_number/numberInputBox';
+import descriptionInputBox from './steps/description/descriptionInputBox';
+import bodyInputBox from './steps/body/bodyInputBox';
+import footerInputBox from './steps/footer/footerInputBox';
+import chosenScope from './steps/scope/chosenScope';
 
 // Example commit
 /**
@@ -46,64 +44,91 @@ export function activate(context: vscode.ExtensionContext) {
 		const DEFAULT_TEMPLATE =  "<type><scope>: <emoji> <number> - <description>\n\n<body>\n\n<footer>";
 		const workspace_template = workspace_config.get('template') as unknown as string;
 
-		// === TYPE ===
-		let type = '';
-		type = await typeQuickPick();
-
-		// === SCOPE ===
-		let scope = null;
-		const saved_scopes: string[] = workspace_config.get('scopes') as unknown as string[];
-
-		const scope_type = await scopeQuickPick(saved_scopes);
-
-		if (scope_type === 'New Scope') {
-			const new_scope = await newScopeInputBox();
-			if (new_scope) {
-				await workspace_config.update('scopes', [...saved_scopes, new_scope], vscode.ConfigurationTarget.Workspace);
-				if (saved_scopes) {
-					scope = [...saved_scopes, new_scope];
-				} else {
-					scope = new_scope;
-				}
-			} else {
-				scope = '';
-			}
-		} else if (scope_type === 'One Time Scope') {
-			const new_scope = await oneTimeScopeInputBox();
-			if (new_scope) {
-				scope = new_scope;
-			} else {
-				scope = '';
-			}
-		} else if (scope_type === 'None') {
-			scope = '';
-		} else {
-			// User chose an existing saved scope.
-			scope = scope_type;
-		}
-
-		// === EMOJI ===
-		const emoji = await emojiQuickPick();
-
-		// === ISSUE/TICKET NUMBER ===
-		const ticket_number = await numberInputBox(issue_number);
-
-		// === DESCRIPTION ===
-		const description = await descriptionInputBox();
-		
-		// === BODY ===
-		const body = await bodyInputBox();
-
-		// === FOOTER ===
-		const footer = await footerInputBox();
-
-		// === BUILDING COMMIT MESSAGE ===
 		let chosen_template = '';
-		if (workspace_template) {
+		if (workspace_template) { // If theres a commit template in the users settings.json file, use that.
 			chosen_template = workspace_template;
 		} else {
 			chosen_template = DEFAULT_TEMPLATE;
 		}
+
+		// === STEP ORDER ===
+		const TEMPLATE_REGEX = /(<)((type|scope|emoji|number|description|body|footer)(>))/g;
+		const matched_values = [...chosen_template.matchAll(TEMPLATE_REGEX)];
+		const step_order = matched_values.map((arr) => arr[0]);
+
+		let type = '';
+		let scope = '';
+		let emoji = '';
+		let ticket_number = '';
+		let description = '';
+		let body = '';
+		let footer = '';
+
+		// === RUN PROMPT'S ===
+		for (let current_step = 0; current_step < step_order.length;) {
+			const step = step_order[current_step];
+			switch (step) {
+				case '<type>':
+					await typeQuickPick()
+							.then((value: string) => {
+								type = value;
+								current_step++;
+							});
+					break;
+				case '<scope>':
+					await chosenScope(workspace_config)
+							.then((value: string) => {
+								scope = value;
+								current_step++;
+							})
+							.catch(() => current_step--);
+					break;
+				case '<emoji>':
+					await emojiQuickPick()
+							.then((value) => {
+								emoji = value;
+								current_step++;
+							})
+							.catch(() => current_step--);
+					break;
+				case '<number>':
+					await numberInputBox(issue_number)
+							.then((value: string) => {
+								ticket_number = value;
+								current_step++;
+							})
+							.catch(() => current_step--);
+					break;
+				case '<description>':
+					await descriptionInputBox()
+							.then((value: string) => {
+								description = value;
+								current_step++;
+							})
+							.catch(() => current_step--);
+					break;
+				case '<body>':
+					await bodyInputBox()
+							.then((value: string) => {
+								body = value;
+								current_step++;
+							})
+							.catch(() => current_step--);
+					break;
+				case '<footer>':
+					await footerInputBox()
+							.then((value: string) => {
+								footer = value;
+								current_step++;
+							})
+							.catch(() => current_step--);
+					break;
+				default:
+					break;
+			}
+		}
+
+		// === BUILDING COMMIT MESSAGE ===
 		const commit_message = chosen_template.replace('<type>', type)
 												.replace('<scope>', scope ? `(${scope})` : '')
 												.replace('<emoji>', emoji)
@@ -111,6 +136,8 @@ export function activate(context: vscode.ExtensionContext) {
 												.replace('<description>', description)
 												.replace('<body>', body)
 												.replace('<footer>', footer);
+
+		console.log('Commit Message:', `\n\n${commit_message}`);
 
 		// === COMMIT ===
 		repo.commit(commit_message)
