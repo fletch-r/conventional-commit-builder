76bedb5da6917735f5ed421d017f8fcedcb4c862
0xATHERIS <ath3ris@proton.me>
2023-04-07 01:49:16 +0100

    style: :recycle: 11 - added comments. use the selected type label for the value.

--- src/steps/type/typeQuickPick.ts
+++ src/steps/type/typeQuickPick.ts
@@ -1,19 +1,24 @@
 import createQuickPick from '../../createQuickPick';
 
 export default async function typeQuickPick(): Promise<string> {
-	return await createQuickPick(
-		'Type',
-		'Select a type for your commit.',
-		[
-			{ label: 'feat', description: 'Feature', detail: "A new feature" },
-			{ label: 'fix', description: 'Bug Fix', detail: 'A bug fix' },
-			{ label: 'style', description: 'Styling', detail: 'Code styling. (formatting, missing semicolons, etc)' },
-			{ label: 'chore', description: 'Chore', detail: 'Changes that do not modify the code' },
-			{ label: 'docs', description: 'Documentation', detail: 'Documentation changes' },
-			{ label: 'test', description: 'Test', detail: "Create or update tests" },
-			{ label: 'refactor', description: 'Code Refactor', detail: 'Code changes that do not change functionality' },
-			{ label: 'build', description: 'Build', detail: 'Changes that affect the build system or dependencies. e.g. NPM' },
-		],
-		1,
+	const available_types = [
+		{ label: 'feat', description: 'Feature', detail: "A new feature" },
+		{ label: 'fix', description: 'Bug Fix', detail: 'A bug fix' },
+		{ label: 'style', description: 'Styling', detail: 'Code styling. (formatting, missing semicolons, etc)' },
+		{ label: 'chore', description: 'Chore', detail: 'Changes that do not modify the code' },
+		{ label: 'docs', description: 'Documentation', detail: 'Documentation changes' },
+		{ label: 'test', description: 'Test', detail: "Create or update tests" },
+		{ label: 'refactor', description: 'Code Refactor', detail: 'Code changes that do not change functionality' },
+		{ label: 'build', description: 'Build', detail: 'Changes that affect the build system or dependencies. e.g. NPM' },
+	];
+
+	const selected_type = await createQuickPick(
+		'Type', // title
+		'Select a type for your commit.', // placeholder
+		available_types, // items
+		1, // step
+		false, // canSelectMany
 	);
+
+	return selected_type[0].label;
 }
