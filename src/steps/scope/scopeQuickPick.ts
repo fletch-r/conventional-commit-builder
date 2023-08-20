fbd5b3469457fbcb9bee9c2ab872bf46d2d19262
fletch-r <ath3ris@proton.me>
2023-08-20 20:52:51 +0100

    refactor: :recycle: #25 - Removed the `as <type>`

--- src/steps/scope/scopeQuickPick.ts
+++ src/steps/scope/scopeQuickPick.ts
@@ -7,16 +7,14 @@ export default async function scopeQuickPick(
 	return new Promise(async (resolve, reject) => {
 		const scope_options = [{ label: 'None', description: 'No scope.' }];
 
-		if (saved_scopes) {
-			const formatted_scopes = saved_scopes.map((str) => ({
-				label: str,
-				description: '',
-				detail: 'From saved scopes.'
-			}));
-			for (let i = 0; i < formatted_scopes.length; i++) {
-				const formatted_scope = formatted_scopes[i];
-				scope_options.push(formatted_scope);
-			}
+		const formatted_scopes = saved_scopes.map((str) => ({
+			label: str,
+			description: '',
+			detail: 'From saved scopes.'
+		}));
+		for (let i = 0; i < formatted_scopes.length; i++) {
+			const formatted_scope = formatted_scopes[i];
+			scope_options.push(formatted_scope);
 		}
 		scope_options.push({ label: 'New Scope', description: 'Add a new scope to your workspace to reuse in the future.' });
 		scope_options.push({ label: 'One Time Scope', description: 'Enter a scope that won\'t get saved to your workspace.' });
