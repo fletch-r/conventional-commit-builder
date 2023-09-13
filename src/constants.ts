28bdbec99cde739afe7e8cfe51807787ed1da568
fletch-r <ath3ris@proton.me>
2023-09-13 22:13:19 +0100

    feat: :sparkles: #33 - re-organised file and added new line default

--- src/constants.ts
+++ src/constants.ts
@@ -1,15 +1,24 @@
+/* === REGEX === */
+
 // Gets the remaining characters after the last /
 export const FILE_NAME_REGEX = /[^\/]+$/;
-
-// You need to escape \ if you want to keep them in the regex output.
-export const DEFAULT_REFERENCE_REGEX = "(?!.*\/)([^\\d]*)(\\d+)";
 
 // Used to replace the steps with what the user entered.
 export const TEMPLATE_REGEX = /(<)((type|scope|emoji|reference|description|body|footer)(>))/g;
 
+/* === STRINGS === */
+
+// You need to escape \ if you want to keep them in the regex output.
+export const DEFAULT_REFERENCE_REGEX = "(?!.*\/)([^\\d]*)(\\d+)";
+
 // Default commit message structure.
 // This will affect in which order the user sees the extension prompts.
 export const DEFAULT_COMMIT_TEMPLATE = "<type><scope>: <emoji> <reference> - <description>\n\n<body>\n\n<footer>";
+
+// Default new line characters that will be replaced with a new line in the commit message.
+export const DEFAULT_NEWLINE = "\\n";
+
+/* === NUMBERS === */
 
 // Total number of prompts.
 export const TOTAL_STEPS = 6;
