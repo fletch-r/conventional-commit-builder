db03e941a32521ebb65b8e005142e4ba65b40c5f
fletch-r <ath3ris@proton.me>
2023-12-24 23:36:03 +0000

    feat: :sparkles: #35 - functionality to transform parts of commit message wrapped in function

---
+++ src/utils/TransformText.ts
@@ -0,0 +1,59 @@
+import { replaceAt } from "./replaceAt";
+
+interface Indexable {
+    [key: string]: any
+}
+
+export type OnlyClassMethods<T> = {
+    [K in keyof T]: 
+    T[K] extends (...args: any[]) => any ?
+    K : never
+}[keyof T];
+
+type TransformTextMethods = OnlyClassMethods<TransformText>;
+
+type PromptToTransform = string;
+type TransformFunctionName = string;
+
+export class TransformText {
+    constructor() {}
+
+    Uppercase(text: string) {
+        return text.toUpperCase();
+    }
+
+    Lowercase(text: string) {
+        return text.toLowerCase();
+    }
+
+    compute(text: string, function_name: string) {
+        const transformed_text = (this as Indexable)[function_name](text);
+        return transformed_text;
+    }
+
+    steps(commit_message_template: string) {
+        const TransformTextMethods = Object.getOwnPropertyNames(TransformText.prototype) as TransformTextMethods[];
+        const usedFunctions = TransformTextMethods.filter((method: TransformTextMethods) => commit_message_template.includes(method));
+
+        const steps_to_transform = new Map<PromptToTransform, TransformFunctionName>();
+
+        let cleaned_commit_template = '';
+
+        usedFunctions.forEach((func) => {
+            const prompt = commit_message_template.split(`${func}(`)[1].split(')')[0];
+            steps_to_transform.set(prompt, func);
+
+            // Remove functions that wrap prompts from commit message template
+            commit_message_template.replace(`${func}(`, '');
+            const startOfPromptWord = commit_message_template.indexOf(`${func}(`) + (func.length + 1);
+            const endOfPromptWord = commit_message_template.indexOf(')', startOfPromptWord);
+            const newMessage = replaceAt(commit_message_template, endOfPromptWord, '');
+            cleaned_commit_template = newMessage.replace(`${func}(`, '');
+        });
+
+        return {
+            steps_to_transform,
+            cleaned_commit_template
+        };
+    }
+}
